f87a02c8bd3eb3563e51bdb3de5afedcbae47caf
deethesaint <vtduong0912@gmail.com>
2023-09-19 16:34:42 +0700

    dictionaries added

--- Loupay_Project/Loupay_Application/Program.cs
+++ Loupay_Project/Loupay_Application/Program.cs
@@ -16,7 +16,7 @@ namespace Loupay_Application
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run();
         }
     }
 }
